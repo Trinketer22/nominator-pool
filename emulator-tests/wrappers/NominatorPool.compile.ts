4a6821798d63b33c22aa1c0e33add691d2be76e6
Trinketer22 <trinketer22@localhost>
2025-04-02 00:07:49 +0300

    TOLK Source

--- emulator-tests/wrappers/NominatorPool.compile.ts
+++ emulator-tests/wrappers/NominatorPool.compile.ts
@@ -1,5 +1,6 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile:CompilerConfig = {
-	targets: ['../func/stdlib.fc','../func/pool.fc']
+        lang: 'tolk',
+	entrypoint: '../func/pool.tolk'
 };
